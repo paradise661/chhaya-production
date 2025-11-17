ac2ce7f8109493a7b5371b9cddec1a11852439e7
aasmitadhakal <dhakalreshma52@gmail.com>
2025-11-18 00:49:10 +0545

    service section made dynamic in home page

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -9,28 +9,20 @@
 
   <div class="container">
       <div class="row align-items-center">
-
           <div class="col-lg-6">
               <div class="hero13-header heading19">
-
                   {{-- Dynamic Small Title --}}
                   <h5>
                       <img src="{{ asset('frontend/assets/img/icons/logo-icons8.svg') }}" alt="">
                       {{ $sliders->title ?? '' }}
                   </h5>
-
                   <div class="space16"></div>
-
                   {{-- Dynamic Main Title --}}
                   <h2>{{ $sliders->short_description ?? '' }}</h2>
-
                   <div class="space24"></div>
-
                   {{-- Dynamic Description --}}
                   <p>{!! $sliders->description ?? 'Default description goes here.' !!}</p>
-
                   <div class="space32"></div>
-
                   <div class="btn-area1">
                       {{-- Dynamic Button 1 --}}
                       <a href="{{ route('frontend.contact')}}" class="header-btn21">
@@ -64,78 +56,80 @@
 <!--===== HERO AREA ENDS =======-->
 <!--===== ABOUT AREA STARTS =======-->
 <div class="about13-section-area sp1">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="images-area" data-aos="zoom-in" data-aos-duration="1000">
-                    <div class="img1">
-                        <img src="{{asset("frontend/assets/img/all-images/new-img/about-img1-h13.png")}}" alt="">
-                    </div>
-                    <div class="img2">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 288 288">
-                            <linearGradient id="imagewave" x1="70.711%" x2="0%" y1="70.711%" y2="0%">
+  <div class="container">
+      <div class="row align-items-center">
+          {{-- LEFT IMAGE AREA --}}
+          <div class="col-lg-6">
+              <div class="images-area" data-aos="zoom-in" data-aos-duration="1000">
+                  {{-- ⭐ Dynamic Main Image --}}
+                  <div class="img1">
+                      <img src="{{ asset(($about_us->banner_image ?? 'default.png')) }}" alt="">
+                  </div>
+                  {{-- ⭐ SVG remains STATIC --}}
+                  <div class="img2">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 288 288">
+                          <linearGradient id="imagewave" x1="70.711%" x2="0%" y1="70.711%" y2="0%">
                               <stop class="stop-color2" offset="0%" stop-opacity="1"></stop>
                               <stop class="stop-color1" offset="100%" stop-opacity="1"></stop>
-                            </linearGradient>
-                            <path fill="url(#imagewave)">
-                            
-                              <animate repeatCount="indefinite" attributeName="d" dur="5s" values="M37.5,186c-12.1-10.5-11.8-32.3-7.2-46.7c4.8-15,13.1-17.8,30.1-36.7C91,68.8,83.5,56.7,103.4,45
-                              c22.2-13.1,51.1-9.5,69.6-1.6c18.1,7.8,15.7,15.3,43.3,33.2c28.8,18.8,37.2,14.3,46.7,27.9c15.6,22.3,6.4,53.3,4.4,60.2
-                              c-3.3,11.2-7.1,23.9-18.5,32c-16.3,11.5-29.5,0.7-48.6,11c-16.2,8.7-12.6,19.7-28.2,33.2c-22.7,19.7-63.8,25.7-79.9,9.7
-                              c-15.2-15.1,0.3-41.7-16.6-54.9C63,186,49.7,196.7,37.5,186z;
-                              
-                              
-                              M51,171.3c-6.1-17.7-15.3-17.2-20.7-32c-8-21.9,0.7-54.6,20.7-67.1c19.5-12.3,32.8,5.5,67.7-3.4C145.2,62,145,49.9,173,43.4
-                              c12-2.8,41.4-9.6,60.2,6.6c19,16.4,16.7,47.5,16,57.7c-1.7,22.8-10.3,25.5-9.4,46.4c1,22.5,11.2,25.8,9.1,42.6
-                              c-2.2,17.6-16.3,37.5-33.5,40.8c-22,4.1-29.4-22.4-54.9-22.6c-31-0.2-40.8,39-68.3,35.7c-17.3-2-32.2-19.8-37.3-34.8
-                              C48.9,198.6,57.8,191,51,171.3z;
-                              
-                              M37.5,186c-12.1-10.5-11.8-32.3-7.2-46.7c4.8-15,13.1-17.8,30.1-36.7C91,68.8,83.5,56.7,103.4,45
-                              c22.2-13.1,51.1-9.5,69.6-1.6c18.1,7.8,15.7,15.3,43.3,33.2c28.8,18.8,37.2,14.3,46.7,27.9c15.6,22.3,6.4,53.3,4.4,60.2
-                              c-3.3,11.2-7.1,23.9-18.5,32c-16.3,11.5-29.5,0.7-48.6,11c-16.2,8.7-12.6,19.7-28.2,33.2c-22.7,19.7-63.8,25.7-79.9,9.7
-                              c-15.2-15.1,0.3-41.7-16.6-54.9C63,186,49.7,196.7,37.5,186z  "></animate>
-                            
-                              </path>
-                            
-                            </svg>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="about13-header heading20">
-                    <h5 data-aos="fade-left" data-aos-duration="800"><img src="assets/img/icons/logo-icons8.svg" alt="">About us</h5>
-                    <div class="space16"></div>
-                    <h2 class="text-anime-style-3">The Minds Behind Your Brand’s Success</h2>
-                    <div class="space16"></div>
-                    <p data-aos="fade-left" data-aos-duration="1000">At the heart of our agency is a passion for creativity and a relentless drive to push boundaries. We are a team of thinkers, dreamers, and doers who specialize in crafting unique strategies bring brands.</p>
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6" data-aos="zoom-in" data-aos-duration="900">
-                            <div class="about-icons">
-                                    <div class="icons">
-                                        <img src="assets/img/icons/about1-h13.svg" alt="">
-                                    </div>
-                                    <a href="about.html">Our Story Creative Innovation</a>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6" data-aos="zoom-in" data-aos-duration="1000">
-                            <div class="about-icons">
-                                    <div class="icons">
-                                        <img src="assets/img/icons/about2-h13.svg" alt="">
-                                    </div>
-                                    <a href="about.html">Driven Creativity Focused </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="space24"></div>
-                    <p data-aos="fade-left" data-aos-duration="1100">Our mission is simple: to help brands stand in a crowded market & connect with their audience in meaningful, lasting ways. We’re not just an agency.</p>
-                    <div class="space32"></div>
-                    <div class="btn-area1" data-aos="fade-left" data-aos-duration="1200">
-                      <a href="contact.html" class="header-btn21">Get Free Quote <i class="fa-solid fa-arrow-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                          </linearGradient>
+                          <path fill="url(#imagewave)">
+                              <animate repeatCount="indefinite" attributeName="d" dur="5s"
+                                  values="M37.5,186c-12.1-10.5-11.8-32.3-7.2-46.7c4.8-15,13.1-17.8,30.1-36.7C91,68.8,83.5,56.7,103.4,45
+                            c22.2-13.1,51.1-9.5,69.6-1.6c18.1,7.8,15.7,15.3,43.3,33.2c28.8,18.8,37.2,14.3,46.7,27.9c15.6,22.3,6.4,53.3,4.4,60.2
+                            c-3.3,11.2-7.1,23.9-18.5,32c-16.3,11.5-29.5,0.7-48.6,11c-16.2,8.7-12.6,19.7-28.2,33.2c-22.7,19.7-63.8,25.7-79.9,9.7
+                            c-15.2-15.1,0.3-41.7-16.6-54.9C63,186,49.7,196.7,37.5,186z;
+
+                            M51,171.3c-6.1-17.7-15.3-17.2-20.7-32c-8-21.9,0.7-54.6,20.7-67.1c19.5-12.3,32.8,5.5,67.7-3.4C145.2,62,145,49.9,173,43.4
+                            c12-2.8,41.4-9.6,60.2,6.6c19,16.4,16.7,47.5,16,57.7c-1.7,22.8-10.3,25.5-9.4,46.4c1,22.5,11.2,25.8,9.1,42.6
+                            c-2.2,17.6-16.3,37.5-33.5,40.8c-22,4.1-29.4-22.4-54.9-22.6c-31-0.2-40.8,39-68.3,35.7c-17.3-2-32.2-19.8-37.3-34.8
+                            C48.9,198.6,57.8,191,51,171.3z;
+
+                            M37.5,186c-12.1-10.5-11.8-32.3-7.2-46.7c4.8-15,13.1-17.8,30.1-36.7C91,68.8,83.5,56.7,103.4,45
+                            c22.2-13.1,51.1-9.5,69.6-1.6c18.1,7.8,15.7,15.3,43.3,33.2c28.8,18.8,37.2,14.3,46.7,27.9c15.6,22.3,6.4,53.3,4.4,60.2
+                            c-3.3,11.2-7.1,23.9-18.5,32c-16.3,11.5-29.5,0.7-48.6,11c-16.2,8.7-12.6,19.7-28.2,33.2c-22.7,19.7-63.8,25.7-79.9,9.7
+                            c-15.2-15.1,0.3-41.7-16.6-54.9C63,186,49.7,196.7,37.5,186z">
+                              </animate>
+                          </path>
+                      </svg>
+                  </div>
+
+              </div>
+          </div>
+          {{-- RIGHT CONTENT AREA STATIC FOR NOW --}}
+          <div class="col-lg-6">
+              <div class="about13-header heading20">
+
+                  <h5 data-aos="fade-left" data-aos-duration="800">
+                      <img src="{{ asset('frontend/assets/img/icons/logo-icons8.svg') }}" alt="">
+                      {{ $about_us->small_title ?? 'About Us' }}
+                  </h5>
+
+                  <div class="space16"></div>
+
+                  <h2 class="text-anime-style-3">
+                      {{ $about_us->title ?? 'Default About Title' }}
+                  </h2>
+
+                  <div class="space16"></div>
+
+                  <p data-aos="fade-left" data-aos-duration="1000">
+                      {!! $about_us->description ?? '' !!}
+                  </p>
+
+                  <div class="space32"></div>
+
+                  <div class="btn-area1" data-aos="fade-left" data-aos-duration="1200">
+                      <a href="{{ $about_us->button_link ?? '#' }}" class="header-btn21">
+                          {{ $about_us->button_text ?? 'Get Free Quote' }}
+                          <i class="fa-solid fa-arrow-right"></i>
+                      </a>
+                  </div>
+
+              </div>
+          </div>
+
+      </div>
+  </div>
 </div>
 <!--===== ABOUT AREA ENDS =======-->
 <!--===== SERVICE AREA STARTS =======-->
@@ -156,107 +150,45 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="800">
-        <div class="service13-boxarea">
-          <div class="icons">
-            <img src="{{asset("frontend/assets/img/icons/service-icons23.svg")}}" alt="">
-          </div>
-          <div class="space32"></div>
-          <div class="content-area">
-            <a href="service1.html" class="head">Advertising Campaign Creation</a>
-            <div class="space16"></div>
-            <p>Our agency offers a comprehensive range of services designed to elevate your brand and maximize its impact. From Brand Strategy</p>
-            <div class="space24"></div>
-            <a href="service1.html" class="readmore">Learn More <i class="fa-solid fa-arrow-right"></i></a>
-            <h2>01</h2>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="900">
-        <div class="service13-boxarea">
-          <div class="icons">
-            <img src="assets/img/icons/service-icons24.svg" alt="">
-          </div>
-          <div class="space32"></div>
-          <div class="content-area">
-            <a href="service1.html" class="head">Brand Strategy & Development</a>
-            <div class="space16"></div>
-            <p>From Brand Strategy & Creative Campaigns to Digital Marketing and Media Planning, we bring a unique blend of innovation expertise</p>
-            <div class="space24"></div>
-            <a href="service1.html" class="readmore">Learn More <i class="fa-solid fa-arrow-right"></i></a>
-            <h2>02</h2>
-          </div>
-        </div>
-      </div>
 
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="1000">
-        <div class="service13-boxarea">
-          <div class="icons">
-            <img src="assets/img/icons/service-icons25.svg" alt="">
+      @foreach($services as $index => $service)
+          <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="{{ 800 + ($index * 100) }}">
+              <div class="service13-boxarea">
+  
+                  {{-- Dynamic Icon --}}
+                  <div class="icons">
+                      <img src="{{ asset( $service->image) }}" alt="">
+                  </div>
+  
+                  <div class="space32"></div>
+  
+                  <div class="content-area">
+  
+                      {{-- Dynamic Service Title --}}
+                      <a href="{{ route('frontend.singleservice', $service->slug) }}" class="head">
+                          {{ $service->title }}
+                      </a>
+  
+                      <div class="space16"></div>
+  
+                      {{-- Dynamic Short Description --}}
+                      <p>{{ Str::limit($service->short_description, 120) }}</p>
+  
+                      <div class="space24"></div>
+  
+                      {{-- Learn More Link --}}
+                      <a href="{{ route('frontend.singleservice', $service->slug) }}" class="readmore">
+                          Learn More <i class="fa-solid fa-arrow-right"></i>
+                      </a>
+                      {{-- Dynamic Number 01, 02, 03 --}}
+                      <h2>{{ str_pad($index + 1, 2, '0', STR_PAD_LEFT) }}</h2>
+                  </div>
+              </div>
           </div>
-          <div class="space32"></div>
-          <div class="content-area">
-            <a href="service1.html" class="head">Targeted Media Buying & Planning</a>
-            <div class="space16"></div>
-            <p>Whether you need targeted social media campaigns, eye-catching visuals, or data-driven performance optimization.</p>
-            <div class="space24"></div>
-            <a href="service1.html" class="readmore">Learn More <i class="fa-solid fa-arrow-right"></i></a>
-            <h2>03</h2>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="1100">
-        <div class="service13-boxarea">
-          <div class="icons">
-            <img src="assets/img/icons/service-icons26.svg" alt="">
-          </div>
-          <div class="space32"></div>
-          <div class="content-area">
-            <a href="service1.html" class="head">Graphic Design & Visual Branding</a>
-            <div class="space16"></div>
-            <p>Each service is tailored meet your specific goals, ensuring that your brand stands out, engages, and drives measurable results</p>
-            <div class="space24"></div>
-            <a href="service1.html" class="readmore">Learn More <i class="fa-solid fa-arrow-right"></i></a>
-            <h2>04</h2>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="1200">
-        <div class="service13-boxarea">
-          <div class="icons">
-            <img src="assets/img/icons/service-icons27.svg" alt="">
-          </div>
-          <div class="space32"></div>
-          <div class="content-area">
-            <a href="service1.html" class="head">Influencer Marketing Partnerships</a>
-            <div class="space16"></div>
-            <p>Whether you need targeted social media campaigns, eye-catching visuals, or data-driven performance optimization.</p>
-            <div class="space24"></div>
-            <a href="service1.html" class="readmore">Learn More <i class="fa-solid fa-arrow-right"></i></a>
-            <h2>05</h2>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration="1300">
-        <div class="service13-boxarea">
-          <div class="icons">
-            <img src="assets/img/icons/service-icons28.svg" alt="">
-          </div>
-          <div class="space32"></div>
-          <div class="content-area">
-            <a href="service1.html" class="head">Video Production & Animation</a>
-            <div class="space16"></div>
-            <p>Our agency offers a comprehensive range of services designed to elevate your brand and maximize its impact. From Brand Strategy</p>
-            <div class="space24"></div>
-            <a href="service1.html" class="readmore">Learn More <i class="fa-solid fa-arrow-right"></i></a>
-            <h2>06</h2>
-          </div>
-        </div>
-      </div>
-    </div>
+      @endforeach
+  
+  </div>
+  
   </div>
 </div>
 <!--===== SERVICE AREA ENDS =======-->
